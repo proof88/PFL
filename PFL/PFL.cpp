68777e2e8a5dc202b92952636ed13d90ee41579a
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2019-06-25 00:03:10 +0200

    First commit to github

---
+++ PFL/PFL.cpp
@@ -0,0 +1,346 @@
+/*
+    ###################################################################################
+    PFL.cpp
+    PR00F Foundation Library
+    This file is part of PFL.
+    Useful functions, macros.
+    Made by PR00F88
+    EMAIL : PR0o0o0o0o0o0o0o0o0o0oF88@gmail.com
+    ###################################################################################
+*/
+
+#include "PFL.h"
+
+// PFL.h already includes string so no use removing the following headers at all to win compilation speed
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define M_PI       
+
+
+// Windows header
+// ------------------------------------------------------------------
+#ifndef WINVER
+//#define WINVER 0x0501
+#define WINVER 0x0600
+#endif
+#ifndef _WIN32_WINNT                  
+#define _WIN32_WINNT 0x0501
+#endif                        
+#ifndef _WIN32_WINDOWS
+#define _WIN32_WINDOWS 0x0410
+#endif
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0600
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+
+using namespace std;
+
+
+// ############################### PUBLIC ################################
+
+
+float PFL::PI = 3.14159265358979323846f;
+
+float PFL::E = 0.0001f;
+
+
+/**
+    Determines whether the given file exists.
+    @return True if a file with the given filename exists, false otherwise.
+*/
+bool PFL::fileExists(const char* filename)
+{
+    return ( GetFileAttributesA(filename) != INVALID_FILE_ATTRIBUTES );
+} // fileExists()
+
+
+/**
+    Extracts the extension from the path.
+    @return The extension in the given filename.
+*/
+string PFL::getExtension(const char* path)
+{
+    const string sf = path;
+    const string::size_type ind = sf.rfind('.');
+    return ( (ind == string::npos) ? "" : string( sf.substr(ind+1) ) );
+} // getExtension()
+
+
+/**
+    Extracts the directory path from the full path.
+    @param path The full path from where the directory path should be extracted.
+    @return The extracted directory path. If not empty, there is always a delimiter character at the end of it.
+            The delimiter character can be slash or backslash, depending on the given path.
+*/
+string PFL::getDirectory(const char* path)
+{
+    string strPath = path;
+    // todo: into new function from here ###
+    const int sizeToCopy = sizeof(char) * (strPath.length()+1);
+    char* const tmpPath = (char*) malloc(sizeToCopy);
+    strcpy_s(tmpPath, sizeToCopy, path);
+    int newLen = strClr(tmpPath);
+    // to here ###
+    if ( newLen == 0 )
+    {
+        free(tmpPath);
+        return "";
+    }
+
+    const bool bHadSlashAtTheEnd = (tmpPath[newLen-1] == '/') || (tmpPath[newLen-1] == '\\');
+    char delimiter = '/';
+    if ( bHadSlashAtTheEnd )
+        delimiter = tmpPath[newLen-1];
+    strClr(tmpPath, '/');
+    newLen = strClr(tmpPath, '\\');
+    if ( newLen == 0 )
+    {
+        free(tmpPath);
+        return "";
+    }
+    strPath = tmpPath;
+    free(tmpPath);
+
+    if ( strchr(strPath.c_str(), '\\') )
+        delimiter = '\\';
+
+    if ( bHadSlashAtTheEnd )
+        strPath = strPath + delimiter;
+    
+    return newLen > 0 ? strPath.substr(0, strPath.find_last_of(delimiter) + 1) : "";
+} // getDirectory()
+
+
+/**
+    Extracts the file name from the path.
+*/
+string PFL::getFilename(const char* path)
+{
+    string strPath = path;
+    // todo: into new function from here ###
+    const int sizeToCopy = sizeof(char) * (strPath.length()+1);
+    char* tmpPath = (char*) malloc(sizeToCopy);
+    strcpy_s(tmpPath, sizeToCopy, path);
+    strClr(tmpPath);
+    strClr(tmpPath, '/', '\\');
+    // to here ###
+    int newLen = strClrTrails(tmpPath, '.');
+    strPath = tmpPath;
+    free(tmpPath);
+    char delimiter = '/';
+    if ( strchr(strPath.c_str(), '\\') )
+        delimiter = '\\';
+    return newLen > 0 ? strPath.substr(strPath.find_last_of(delimiter) + 1) : "";
+} // getFilename()
+
+
+/**
+    Changes the file extension in the given path.
+*/
+string PFL::changeExtension(const char* path, const char* ext)
+{
+    const string strFilename = getFilename(path);
+    return strFilename.length() > 0 ? 
+        getDirectory(path) + strFilename.substr(0, strFilename.find_last_of('.')) + '.' + ext : "";
+} // changeExtension()
+
+
+/**
+    Gets how many times the given character appears in the given buffer.
+    @return Number of appearance of the given character in the given buffer.
+*/
+unsigned int PFL::numCharAppears(char searchFor, const char* buffer, size_t buffer_size)
+{
+    if ( buffer == NULL )
+        return 0;
+
+    unsigned int times = 0;
+    const char* srchbuffer = buffer;
+    while ( srchbuffer < (char*) buffer + buffer_size ) 
+    {
+        if ( *srchbuffer == searchFor )
+            times++;
+        srchbuffer++;
+    }
+
+    return times;
+} // numCharAppears()
+
+
+/**
+    Removes trailing chars from the given string.
+    Any trailing char can be removed from the end of the string.
+    If not specified, the default trailing chars to be removed are space and tab chars.
+    @param srcStr      The string to be modified.
+    @param targetChar1 First char to be removed.
+    @param targetChar2 Second char to be removed.
+    @return The string's new length which is never greater than the original length.
+*/
+unsigned int PFL::strClrTrails(char* srcStr, char targetChar1, char targetChar2)
+{
+    int newStrLen = strlen(srcStr);
+    char* p = &srcStr[newStrLen-1];
+    while ( (*p == targetChar1 || *p == targetChar2) && p >= srcStr)
+    {
+        *p = '\0';
+        p--;
+        newStrLen--;
+    }    
+    return newStrLen;
+} // strClrTrails()
+
+
+/**
+    Removes leading chars from the given string.
+    Any leading char can be removed from the beginning of the string.
+    If not specified, the default leading chars to be removed are space and tab chars.
+    @param srcStr      The string to be modified.
+    @param targetChar1 First char to be removed.
+    @param targetChar2 Second char to be removed.
+    @return The string's new length which is never greater than the original length.
+*/
+unsigned int PFL::strClrLeads(char* const srcStr, char targetChar1, char targetChar2)
+{
+    int origLen;
+    int newStrLen = origLen = strlen(srcStr);
+    char* p = srcStr;
+    while ( (*p == targetChar1 || *p == targetChar2) && p < srcStr+origLen)
+    {
+        p++;
+        newStrLen--;
+    }   
+    if ( p != srcStr )
+    {
+        int i = 0;
+        while ( p[i] != '\0' )
+        {
+            srcStr[i] = p[i];
+            i++;
+        }
+        srcStr[i] = '\0';
+    }
+    return newStrLen;
+} // strClrLeads()
+
+
+/**
+    Removes leading and trailing chars from the given string.
+    Any leading and trailing char can be removed from the beginning and the end of the string.
+    If not specified, the default chars to be removed are space and tab chars.
+    @param srcStr      The string to be modified.
+    @param targetChar1 First char to be removed.
+    @param targetChar2 Second char to be removed.
+    @return The string's new length which is never greater than the original length.
+*/
+unsigned int PFL::strClr(char* const srcStr, char targetChar1, char targetChar2)
+{
+    strClrLeads(srcStr, targetChar1, targetChar2);
+    return strClrTrails(srcStr, targetChar1, targetChar2);
+} // strClr()
+
+
+/**
+    Returns PI.
+*/
+float PFL::pi()
+{
+    return PFL::PI;
+} // pi()
+
+
+/**
+    Rounds the given value to the nearest whole number.
+    @return The rounded value as a float.
+*/
+float PFL::roundf(float value)
+{
+    return value < 0.0f ? ceil(value - 0.5f) : floor(value + 0.5f);
+} // roundf()
+
+
+/**
+    Rounds the given value to the nearest whole number.
+    @return The rounded value as a signed integer.
+*/
+int PFL::roundi(float value)
+{
+  return (int) roundf(value);
+} // roundi()
+
+
+/**
+    Constrains the given value into the given [min,max] bounds.
+*/
+float PFL::constrain(float value, float min, float max)
+{
+    if ( value < min )
+        return min;
+    else if ( value > max )
+        return max;
+
+    return value;
+} // constrain()
+
+
+/**
+    Converts the given angle from degrees to radians.
+    @return The given angle in radians.
+*/
+float PFL::degToRad(float degree)
+{
+    return degree * PFL::PI / 180.0f;
+} // degToRad()
+
+
+/**
+    Converts the given angle from radians to degrees.
+    @return The given angle in degrees.
+*/
+float PFL::radToDeg(float radian)
+{
+    return radian * 180.0f / PFL::PI;
+} // radToDeg()
+
+
+PFL::PFL()
+{
+
+}
+
+
+PFL::PFL(const PFL& other)
+{
+
+}
+
+
+PFL& PFL::operator= (const PFL& other)
+{
+    return *this;
+}
+
+
+PFL::~PFL()
+{
+
+}
+
+
+// ############################## PROTECTED ##############################
+
+
+// ############################### PRIVATE ###############################
+
+
+
+
+
+
---
+++ PFL/PFL.h
@@ -0,0 +1,188 @@
+#pragma once
+
+/*
+    ###################################################################################
+    PFL.h
+    PR00F Foundation Library
+    This file is part of PFL.
+    Made by PR00F88
+    EMAIL : PR0o0o0o0o0o0o0o0o0o0oF88@gmail.com
+    ###################################################################################
+*/
+
+#include <string>
+
+/**
+    Bit manipulation macros.
+    Good article: https://www.coranac.com/documents/working-with-bits-and-bitfields/ written by Jasper Vijn (cearn@coranac.com).
+*/
+
+/**
+    Sets the specified bit b to 1 in value v.
+
+    @return Changed value v after set operation.
+*/
+#define BIT_SET(v,b) ( v |= ( 1 << (b) ) )
+
+
+/**
+    Sets the specified bit b to 0 in value v.
+
+    @return Changed value v after clear operation.
+*/
+#define BIT_CLEAR(v,b) ( v &= ~( 1 << (b) ) )
+
+
+/**
+    Gets value of bit b of value v.
+
+    @return Value of bit b of value v.
+*/
+#define BIT_READ(v,b) ( (0u == ( v & ( 1 << (b) ) ) ) ? 0u : 1u )
+
+
+/**
+    Toggles/flaps/flips/inverts bit b of value v.
+
+    @return Changed value v after toggle operation.
+*/
+#define BIT_TOGGLE(v,b) ( v ^= ( 1 << (b) ) )
+
+
+/**
+    Creates a value where only the specified bit n is set to 1.
+
+    @return Value where only bit n is set to 1 (2^n).
+*/
+#define BIT(n) ((unsigned)( 1 << (n) ))
+
+
+/**
+    Bitfield manipulation macros.
+*/
+
+
+/**
+    Creates a bitmask where n bits starting from bit 0 are 1.
+
+    Example: BIT_MASK(3) = 111
+
+    @return Bitmask where n bits starting from bit 0 are 1 (2^n-1).
+*/
+#define BIT_MASK(n) ( BIT(n)-1 )
+
+
+/**
+    Creates a bitfield mask where n bits starting from s are 1.
+
+    Example: BITF_MASK(2,3) = 11100
+
+    @return Bitfield mask where n bits starting from s are 1.
+*/
+#define BITF_MASK(s,n) ( BIT_MASK(n) << (s) )
+
+
+/**
+    Gets the specified n-length bitfield starting at bit s from value v.
+
+    @return Value of n-length bitfield starting at bit s from value v.
+*/
+#define BITF_READ(v,s,n) ( ( (v) >> (s) ) & BIT_MASK(n) )
+
+
+/**
+    Prepare an n-length bitmask starting at bit s for combining by setting it to x.
+
+    Example: BITF_PREP(6,2,3) = ( ((6) & BIT_MASK(3)) << (2) ) =
+                              = ( ((110) & 111) << (2) ) =
+                              = ( 110 << (2) ) =
+                              = ( 11000 )
+
+    @param x Value to be stored (our input bitfield).
+    @param s At which bit the value is stored in the return value.
+    @param n Length of the input bitfield.
+
+    @return A value where the n-length bitfield starting at bit s is set to x.
+*/
+#define BITF_PREP(x,s,n) ( ((x) & BIT_MASK(n)) << (s) )
+
+
+/**
+    Sets the specified n-length bitfield starting at bit s in value v to x.
+
+    Example:
+    v = 0
+    BITF_SET(v,3,2,3) = ( v=((3) &~ BITF_MASK(2,3)) | BITF_PREP(6,2,3) ) =
+                      = ( v=((11) &~ 11100) | 11000 ) =
+                      = ( v=(11 & 00011) | 11000 ) =
+                      = ( v= 11 | 11000 ) = 11011
+                      https://www.coranac.com/documents/working-with-bits-and-bitfields/
+
+    @param v Target variable where we are writing our bitfield to.
+    @param x Value to be stored (our input bitfield).
+    @param s At which bit the value is stored in v.
+    @param n Length of the input bitfield.
+
+    @return Changed value v where the n-length bitfield starting at bit s is set to x.
+*/
+#define BITF_SET(v,x,s,n) ( v=((v) &~ BITF_MASK(s,n)) | BITF_PREP(x,s,n) )
+
+
+/**
+    PR00F Foundation Library class.
+    Useful functions.
+
+    Not following the Pimpl idiom here as there are no hidden members in this class for the outside world.
+
+    This class uses WinAPI directly.
+*/
+class PFL
+{
+
+public:
+    static float PI;
+    static float E;
+
+    // ---------------------------------------------------------------------------
+
+    static bool         fileExists(const char* path);     /**< Determines whether the given file exists. */
+    static std::string  getExtension(const char* path);   /**< Extracts the extension from the path. */
+    static std::string  getDirectory(const char* path);   /**< Extracts the directory from the path. */
+    static std::string  getFilename(const char* path);    /**< Extracts the file name from the path. */
+    static std::string  changeExtension(
+        const char* path, const char* ext);               /**< Changes the file extension in the given path. */
+
+    static unsigned int numCharAppears(
+        const char searchFor, const char* buffer,
+        size_t buffer_size);                              /**< Gets how many times the given character appears in the given buffer. */
+
+    static unsigned int strClrTrails(
+        char* srcStr,
+        char targetChar1 = ' ', char targetChar2 = '\t'); /**< Removes trailing spaces and tabs from the given string. */
+    static unsigned int strClrLeads(
+        char* srcStr,
+        char targetChar1 = ' ', char targetChar2 = '\t'); /**< Removes leading spaces and tabs from the given string. */
+    static unsigned int strClr(
+        char* srcStr,
+        char targetChar1 = ' ', char targetChar2 = '\t'); /**< Removes leading and trailing spaces and tabs from the given string. */
+
+    static float pi();                          /**< Returns PI. */
+
+    static float roundf(float value);           /**< Rounds the given value to the nearest whole number. */
+    static int   roundi(float value);           /**< Rounds the given value to the nearest whole number. */
+
+    static float constrain(
+        float value, float min, float max);     /**< Constrains the given value into the given [min,max] bounds. */
+
+    static float degToRad(float degree);        /**< Converts the given angle from degrees to radians. */
+    static float radToDeg(float radian);        /**< Converts the given angle from radians to degrees. */
+
+    // ---------------------------------------------------------------------------
+
+    PFL(); 
+    PFL(const PFL& other);
+    PFL& operator= (const PFL& other);
+    virtual ~PFL();
+
+}; // class PFL
+
